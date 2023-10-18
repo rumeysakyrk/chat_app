e701f6623bdf7bd2c3aea58808c1b073b5ca750b
rumeysakyrk <rumeysakyrk@gmail.com>
2023-10-18 16:48:42 +0300

    message forwarding and socket API

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import {useEffect} from 'react'
 import ChatForm from './ChatForm'
 import ChatList from './ChatList'
 
+import {init,subscribeChat} from "../socketApi"
+import {useChat} from "../context/ChatContext"
+
 function Container() {
+    const {setMessages} =useChat();
+    useEffect(() => {
+        init();
+        subscribeChat((message) => {
+            setMessages((prevState) => [...prevState, {message}]);
+        });
+    }, [])
+    
     return (
         <div>
             <ChatList />
